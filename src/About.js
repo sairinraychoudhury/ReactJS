d4a2a8b6c5482897cb51cf306f88af12fd3a2314
sairinraychoudhury <sairinraychoudhury@gmail.com>
2024-04-25 16:23:52 +0530

    Commit-01

---
+++ src/About.js
@@ -0,0 +1,12 @@
+
+
+const About = () => {
+    return (
+        <main className='About'>
+            <h2>About</h2>
+            <p style={{ marginTop: "1rem" }}>This is a blog app.</p>
+        </main>
+    )
+}
+
+export default About
